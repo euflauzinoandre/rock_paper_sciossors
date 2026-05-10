2c5917ea7059dd5d232c5b20e6dafe76718b5c35
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2026-05-10 11:05:27 +0100

    Create getHuumanChoice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
-//Logic to Computer Choice
 function getComputerChoice() {
 	const choice = ["ROCK", "PAPER", "SCISSORS"];
 	return choice[Math.floor(Math.random() * choice.length)];
 }
+
+function getHumanChoice(){
+	const choice = prompt("Write your choice: ").toUpperCase();
+	return choice;
+}
+`
