44c6c94d0a9e907d6e62b615c998d9163a7c34db
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2026-05-10 10:50:53 +0100

    Updated getComputerChoice function with just one variable

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 //Logic to Computer Choice
 function getComputerChoice() {
 	const choice = ["ROCK", "PAPER", "SCISSORS"];
-	const randomChoice = choice[Math.floor(Math.random() * choice.length)];
-
-	return randomChoice;
+	return choice[Math.floor(Math.random() * choice.length)];
 }
